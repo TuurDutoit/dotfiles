ac10615091adc7cf97d8b6cb3d7fc11b992ce69b
Tuur Dutoit <tuur@datacamp.com>
2020-01-07 12:46:43 +0100

    Remove hyper and iTerm2

--- iterm2/install.sh
+++
@@ -1,5 +0,0 @@
-
-# Install the custom themes for iTerm.
-open $DOTFILES/"iterm2/themes/material-design-colors.itermcolors"
-open $DOTFILES/"iterm2/themes/nuno-blue.itermcolors"
-open $DOTFILES/"iterm2/themes/nuno.itermcolors"
