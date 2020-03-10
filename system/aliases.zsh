bb4bab95230ff8d63f3a3a2fae9a750326415797
Tuur Dutoit <tuur@datacamp.com>
2020-03-10 15:46:21 +0100

    Make l and la list all files

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,9 +4,9 @@
 if $(gls &>/dev/null)
 then
   alias ls="gls -F --color"
-  alias l="gls -lAh --color"
+  alias l="gls -lah --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias la='gls -a --color'
 fi
 
 # Add macos alias
