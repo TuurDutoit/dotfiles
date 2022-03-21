ddc0d3629207822db0480aad6e14ec5629bd7d84
Tuur Dutoit <tuur@datacamp.com>
2022-03-21 13:12:14 +0100

    fix(node): Improve node symlink

--- modules/node/setup.zsh
+++ modules/node/setup.zsh
@@ -1,1 +1,9 @@
-sudo ln -s "$(brew --prefix node)/bin/node" /usr/local/bin/node
+DEST="/usr/local/bin/node"
+
+if [ -e "$DEST" ]; then
+  success "node symlink already installed"
+else
+  info "Installing node symlink (requires password)"
+  sudo ln -s "$(brew --prefix node)/bin/node" /usr/local/bin/node
+  success "node symlink installed"
+fi
