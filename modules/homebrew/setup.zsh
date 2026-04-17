7e05c009213eb8c82a664f55b4f3a0507ccd7c9e
Tuur Dutoit <tuur@datacamp.com>
2026-04-17 17:27:10 +0200

    Update Homebrew install command

--- modules/homebrew/setup.zsh
+++ modules/homebrew/setup.zsh
@@ -3,15 +3,8 @@ info "installing homebrew"
 if test ! $(which brew)
 then
   info "homebrew not yet installed"
-
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-  fi
+  
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   success "homebrew installed successfully"
 else
